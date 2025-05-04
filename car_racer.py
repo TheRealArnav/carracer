4b7e3a1461e0229ff62ccb070265f1b9f3a309b6
TheRealArnav <tyagi.arnav02@gmail.com>
2025-05-04 14:07:35 +0200

    Add files via upload

--- car_racer.py
+++ car_racer.py
@@ -28,25 +28,83 @@ carx = 135
 cary = 400
 car_rect = pygame.Rect(carx,cary,45,75) 
 
+coin = pygame.image.load("C:/Pygame2/images/coin.png")
+coin = pygame.transform.scale(coin,(35,35))
+coinx = 335
+coiny = -45
+coin_rect = pygame.Rect(coinx,coiny,35,35) 
+
+rock = pygame.image.load("C:/Pygame2/images/rock.png")
+rock = pygame.transform.scale(rock,(35,35))
+rockx = 135
+rocky = -45
+rock_rect = pygame.Rect(rockx,rocky,35,35)
+
 clock = pygame.time.Clock()
 
 run = True
 
+velocity = 1.5
+
+font = pygame.font.SysFont("comicsans",45)
+
+score = 0
+
 while run == True:
-    clock.tick(375)
+
+    Score = font.render("Score: "+str(score),1,"white")
+    coin_rect = pygame.Rect(coinx,coiny,35,35)
+    car_rect = pygame.Rect(carx,cary,45,75)  
+    rock_rect = pygame.Rect(rockx,rocky,35,35)
+    clock.tick(100)
     screen.blit(road,(roadx,roady))
     screen.blit(sidebars,(sidebarx,sidebary))
     screen.blit(sidebars2,(siderbar2x,sidebar2y))
     screen.blit(car,(carx,cary))
-    sidebary = sidebary + 1.5
-    sidebar2y = sidebar2y + 1.5
-    roady = roady + 1.5
-
+    screen.blit(rock,(rockx,rocky))
+    screen.blit(coin,(coinx,coiny))
+    screen.blit(Score,(90,45))
+    sidebary = sidebary + velocity
+    sidebar2y = sidebar2y + velocity
+    roady = roady + velocity
+    coiny = coiny + velocity
+    rocky = rocky + velocity
 
     if sidebary > 0 or sidebar2y > 0 or roady > 0:
         sidebary = -190
         sidebar2y = -190
         roady = -150
+    if coin_rect.colliderect(car_rect):
+        coiny = -35
+        score = score + 1
+        j = random.randint(1,2)
+        if j == 1:
+            coinx = 135
+        if j == 2:
+            coinx = 335
+    if coiny > 500:
+        coiny = -35
+        j = random.randint(1,2)
+        if j == 1:
+            coinx = 135
+        if j == 2:
+            coinx = 335
+
+    if rock_rect.colliderect(car_rect):
+        rocky = -35
+        score = score - 1
+        r = random.randint(1,2)
+        if r == 1:
+            rockx = 135
+        if r == 2:
+            rockx = 335
+    if rocky > 500:
+        rocky = -35
+        r = random.randint(1,2)
+        if r == 1:
+            rockx = 135
+        if r == 2:
+            rockx = 335
     for event in pygame.event.get():
         if event.type == QUIT:
             quit()
